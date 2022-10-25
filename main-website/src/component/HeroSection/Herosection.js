e46c6714099a5689d00dc250a8a9362d534d97ca
DevvSakib <devvsakib@gmail.com>
2022-10-25 23:25:08 +0600

    cool update

--- main-website/src/component/HeroSection/Herosection.js
+++ main-website/src/component/HeroSection/Herosection.js
@@ -3,16 +3,14 @@ import "./hero.css"
 function Herosection() {
   return (
     <>
-      <div className='hero-section flex items-center'>
-        <div className='hero-content flex-auto'>
+      <div className='hero-section flex py-10 items-center '>
+        <div className='hero-content pb-10'>
           <h2 className=' text-5xl font-bold text'>PoweTheWeb</h2>
           <p className='mt-5 text-xl'>An opensouce Projects for Web Developer. Start your development from here.</p>
         </div>
         <div className='dc-frame'> 
           <iframe
             src="https://discord.com/widget?id=1014618195174363157&theme=dark"
-            width="600"
-            height="400"
             allowtransparency="true"
             frameborder="0"
             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
